d2bfc0c6722bbad4c096c036e7b41e042094f973
jsy8315 <tdhitbtd@gmail.com>
2024-03-07 06:52:22 +0900

    백준 20291완료_dict.get() & dict.items()

--- 20291_02.py
+++ 20291_02.py
@@ -9,8 +9,6 @@ for i in range(N):
     file = sys.stdin.readline().strip().split('.')
     arr.append(file[1])
 
-print(arr)
-
 for i in arr:
     if dict.get(i): #dict에 arr[i]가 존재하면
         dict[i] += 1
@@ -18,7 +16,6 @@ for i in arr:
         dict[i] = 1
 
 dict = sorted(dict.items())
-print(dict)
 
 for i, j in dict:
     print(i, j)
