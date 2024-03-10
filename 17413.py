cc149e42190ef67ff350c754fd06136338de949a
jsy8315 <tdhitbtd@gmail.com>
2024-03-10 10:56:59 +0900

    17413_성공_pop, append 사용

--- 17413.py
+++ 17413.py
@@ -1,32 +1,44 @@
 import sys
 
-S = sys.stdin.readline().strip()
-
-print(S)
-print(len(S))
-print(type(S))
-
-tagNum = 0  #태그의 갯수
-
-for i in range(len(S)):
-    if S[i] == "<":
-        tagNum += 1
-
-print(tagNum)
-
-r = ["a" for _ in range(len(S))]
-print(r)
+S = list(sys.stdin.readline().strip())
+S.append(" ")
 
 checkTag = 0
+word = []
+justword = []
 
-for i in range(len(S)):
-    if S[i] == '<':
+while len(S) != 0:
+    if S[0] == "<":
         checkTag = 1
-        r[i] = S[i]
-    if checkTag == 1:
-        r[i] = S[i]
-    if S[i] == '>':
-        checkTag = 0
-        r[i]
+        for i in range(len(justword)):
+            word.append(justword.pop())
+        
+        justword.append(S.pop(0))
 
+    elif S[0] == ">":
+        checkTag = 0
+        justword.append(S.pop(0))
+        for i in range(len(justword)):
+            word.append(justword.pop(0))
+
+    elif S[0] == " " and checkTag == 0:
+        for i in range(len(justword)):
+            word.append(justword.pop())
+        word.append(" ")
+        S.pop(0)
+
+    elif S[0] == " " and len(S) == 1:
+        for i in range(len(justword)):
+            word.append(justword.pop())
+        word.append(" ")
+        S.pop(0)
+
+    else:
+        justword.append(S.pop(0))
+
+for i in range(len(word)):
+    if i != len(word) - 1:
+        print(word[i], end = "")
+    else:
+        print(word[i])
 
